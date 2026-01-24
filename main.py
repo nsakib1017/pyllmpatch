4f333489c87c789cf46245cbf29ad127f0d0196f
Nazmus Sakib <nsakib1017@gmail.com>
2026-01-24 15:22:08 -0600

    added local idx

--- main.py
+++ main.py
@@ -424,7 +424,7 @@ if __name__ == "__main__":
                         error_description=initial_error_description,
                         log_base=LOG_BASE,
                         file_hash=file_hash,
-                        llm=OPEN_LLM_MODELS[4],
+                        llm=OPEN_LLM_MODELS[int(os.getenv("LOCAL_LLM_IDX"))] if os.getenv("LOCAL_LLM_IDX") and int(os.getenv("LOCAL_LLM_IDX")) < len(OPEN_LLM_MODELS) else OPEN_LLM_MODELS[0],
                         log_rec=log_rec,
                         strategy_state={"syntax_context": {"failures": 0}, "whole_file": {"failures": 0}},
                         try_whole_file=True if ((total_attempts_completed > (int(max_retries/2) - 1))) and (outer_idx==2) else False,
