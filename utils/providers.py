b9c35e36f6500f9e7a40224866fff768cec47fa3
Nazmus Sakib <nsakib1017@gmail.com>
2026-01-26 15:22:15 +0000

    added llama

--- utils/providers.py
+++ utils/providers.py
@@ -37,6 +37,7 @@ OPEN_LLM_MODELS =  [
     {'provider': 'DeepSeek', 'name': 'DeepSeek-R1-0528-Qwen3-8B', 'token_for_completion': 32768, 'model_path': f"{os.getenv('PROJECT_ROOT_DIR')}/finetuning/finetuned_models/unsloth/DeepSeek-R1-0528-Qwen3-8B/run_1769125567/checkpoint-300"},
     {'provider': 'Microsoft', 'name': 'phi-4', 'token_for_completion': 16384, 'model_path': f"{os.getenv('PROJECT_ROOT_DIR')}/finetuning/finetuned_models/unsloth/Phi-4-unsloth-bnb-4bit/run_1769294194/checkpoint-300"},
     {'provider': 'OpenAI', 'name': 'gpt-oss-20b', 'token_for_completion': 32768, 'model_path': f"{os.getenv('PROJECT_ROOT_DIR')}/finetuning/finetuned_models/unsloth/gpt-oss-20b/run_1769125385/checkpoint-800"},
+    {'provider': 'Meta', 'name': 'llama-3.3-70b', 'token_for_completion': 32768, 'model_path': f"{os.getenv('PROJECT_ROOT_DIR')}/finetuning/finetuned_models/unsloth/Llama-3.3-70B-Instruct-bnb-4bit/run_1769058870/checkpoint-200"},
     # {'provider': 'Google', 'name': 'gemma-3-12b-it-unsloth-bnb-4bit', 'token_for_completion': 32768, 'model_path': f"{os.getenv('PROJECT_ROOT_DIR')}/finetuning/finetuned_models/unsloth/gemma-3-12b-it-unsloth-bnb-4bit/run_1769095208/checkpoint-200"}
 ]
 
